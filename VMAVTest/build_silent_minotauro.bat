e8faad59b36d38225a992d6dafe38444daaca4d2
Zeno <zeno@hackingteam.com>
2013-02-22 14:36:07 +0100

    elite e melt

--- VMAVTest/build_silent_minotauro.bat
+++ VMAVTest/build_silent_minotauro.bat
@@ -1,2 +1,2 @@
 cd c:\users\avtest\desktop\avtest
-c:\users\avtest\desktop\avtest\lib\vmavtest.py scout -b 192.168.100.201 -f 192.168.100.204 -k silent 
+c:\users\avtest\desktop\avtest\lib\vmavtest.py elite -b 192.168.100.201 -f 192.168.100.204 -k silent 
