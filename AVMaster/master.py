d0dd7facb0c39b309236bf0278d9b087ec947ddd
olli <olli@hackingteam.it>
2013-02-22 09:39:26 +0100

    identing

--- AVMaster/master.py
+++ AVMaster/master.py
@@ -21,30 +21,30 @@ op_conf_file = os.path.join("conf", "operations.cfg")
 vmman = VMManagerVS(vm_conf_file)
 
 def update(vm_name):
-        try:
-            vm = VMachine(vm_conf_file, vm_name)
-            vmman.revertSnapshot(vm, vm.snapshot)
-            sleep(random.randint(10,60))
-            vmman.startup(vm)
-            sleep(random.randint(60,2*60))
-            vmman.reboot(vm)
-            # executing scripts for vm and wait 3 hours
-            print "[%s] waiting for Updatess" % vm_name
-            sleep(50 * 60)
-            sleep(random.randint(10,600))
-            print "[%s] Shutdown for reconfigurations" % vm_name
-            vmman.shutdown(vm)
-            sleep(30 * 60)
-            print "[%s] Startup" % vm_name
-            vmman.startup(vm)
-            sleep(10 * 60)
-            print "[%s] Suspending and saving new snapshot" % vm_name
-            vmman.suspend(vm)
-            sleep(30)
-            vmman.refreshSnapshot(vm)
-            return "[%s] Updated!"  % vm_name
-        except Exception as e:
-            return "ERROR: %s is not updated. Reason: %s" % (vm_name, e)
+    try:
+        vm = VMachine(vm_conf_file, vm_name)
+        vmman.revertSnapshot(vm, vm.snapshot)
+        sleep(random.randint(10,60))
+        vmman.startup(vm)
+        sleep(random.randint(60,2*60))
+        vmman.reboot(vm)
+        # executing scripts for vm and wait 3 hours
+        print "[%s] waiting for Updatess" % vm_name
+        sleep(50 * 60)
+        sleep(random.randint(10,600))
+        print "[%s] Shutdown for reconfigurations" % vm_name
+        vmman.shutdown(vm)
+        sleep(30 * 60)
+        print "[%s] Startup" % vm_name
+        vmman.startup(vm)
+        sleep(10 * 60)
+        print "[%s] Suspending and saving new snapshot" % vm_name
+        vmman.suspend(vm)
+        sleep(30)
+        vmman.refreshSnapshot(vm)
+        return "[%s] Updated!"  % vm_name
+    except Exception as e:
+        return "ERROR: %s is not updated. Reason: %s" % (vm_name, e)
 
 
 def revert(vm_name):
@@ -101,62 +101,62 @@ def test_internet(vm_name):
  
 
 def dispatch(vm_name):
-        print "go dispatch " , vm_name
-        try:
-                vm = VMachine(vm_conf_file, vm_name)
-                vmman.revertSnapshot(vm, vm.snapshot)
-                sleep(5)
-                vmman.startup(vm)
-                sleep(5)
+    print "go dispatch " , vm_name
+    try:
+        vm = VMachine(vm_conf_file, vm_name)
+        vmman.revertSnapshot(vm, vm.snapshot)
+        sleep(5)
+        vmman.startup(vm)
+        sleep(5)
 
-                test_dir = "C:\\Users\\avtest\\Desktop\\AVTEST"
-                lib_dir = "%s\\lib" % test_dir
-                assets_dir = "%s\\assets" % test_dir
-                vmavtest = "../VMAVTest/"
+        test_dir = "C:\\Users\\avtest\\Desktop\\AVTEST"
+        lib_dir = "%s\\lib" % test_dir
+        assets_dir = "%s\\assets" % test_dir
+        vmavtest = "../VMAVTest/"
 
-                vmman.mkdirInGuest(vm, test_dir)
+        vmman.mkdirInGuest(vm, test_dir)
 
-                filestocopy =[  "./build_silent_minotauro.bat",
-                                "lib/vmavtest.py",
-                                "lib/logger.py",
-                                "lib/rcs_client.py",
-                                "assets/config.json",
-                                "assets/keyinject.exe",
-                                "assets/meltapp.exe"    ]
-                memo = []
-                for filetocopy in filestocopy:
-                        d,f = filetocopy.split("/")
+        filestocopy =[  "./build_silent_minotauro.bat",
+                        "lib/vmavtest.py",
+                        "lib/logger.py",
+                        "lib/rcs_client.py",
+                        "assets/config.json",
+                        "assets/keyinject.exe",
+                        "assets/meltapp.exe"    ]
+        memo = []
+        for filetocopy in filestocopy:
+            d,f = filetocopy.split("/")
 
-                        if d == ".":
-                            src = "%s/%s" % (vmavtest,f)
-                            dst =  "%s\\%s" % (test_dir,f)
-                        else:
-                            src = "%s/%s/%s" % (vmavtest,d,f)
-                            dst =  "%s\\%s\\%s" % (test_dir,d,f)
-                            rdir = "%s\\%s" % (test_dir,d)
-                            if not memo.__contains__(rdir):
-                                    vmman.mkdirInGuest( vm, rdir )
-                                    memo.append( rdir )
+            if d == ".":
+                src = "%s/%s" % (vmavtest,f)
+                dst =  "%s\\%s" % (test_dir,f)
+            else:
+                src = "%s/%s/%s" % (vmavtest,d,f)
+                dst =  "%s\\%s\\%s" % (test_dir,d,f)
+                rdir = "%s\\%s" % (test_dir,d)
+                if not memo.__contains__(rdir):
+                        vmman.mkdirInGuest( vm, rdir )
+                        memo.append( rdir )
 
-                        print src, dst
-                        vmman.copyFileToGuest(vm, src, dst)
+            print src, dst
+            vmman.copyFileToGuest(vm, src, dst)
 
-                # executing bat synchronized
-                vmman.executeCmd(vm, "%s\\build_silent_minotauro.bat" % test_dir )
-                
-                # save results.txt locally
-                save_results(vm)
+        # executing bat synchronized
+        vmman.executeCmd(vm, "%s\\build_silent_minotauro.bat" % test_dir )
+        
+        # save results.txt locally
+        save_results(vm)
 
-                # suspend & refresh snapshot
-                vmman.suspend(vm)
-                #sleep(5)
-                #vmman.refreshSnapshot(vm, vm.snapshot)
-                
-                return "[%s] test files dispatched" % vm_name
+        # suspend & refresh snapshot
+        vmman.suspend(vm)
+        #sleep(5)
+        #vmman.refreshSnapshot(vm, vm.snapshot)
+        
+        return "[%s] test files dispatched" % vm_name
 
-        except Exception, ex:
-                print "exception inside ", ex
-                return "Error: cannot dispatch tests for %s" % vm_name
+    except Exception, ex:
+            print "exception inside ", ex
+            return "Error: cannot dispatch tests for %s" % vm_name
 
 
 def save_results(vm):
@@ -172,51 +172,51 @@ def save_results(vm):
     return "%s %s" % (vm, last)
 
 def main():
-        lib.logger.setLogger()
+    lib.logger.setLogger()
 
-        parser = argparse.ArgumentParser(description='AVMonitor master.')
-    
-        parser.add_argument('action', choices=['update', 'revert', 'dispatch', 'test', 'test_internet'],
-            help="The operation to perform")
-        parser.add_argument('--vm', required=False, 
-            help="Virtual Machine where execute the operation")
-        parser.add_argument('-p', '--pool', default=2, type=int, 
-            help="This is the number of parallel process (default 2)")
-        args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='AVMonitor master.')
 
-        if args.action == "test":
-            get_results("eset")
-            exit(0)
+    parser.add_argument('action', choices=['update', 'revert', 'dispatch', 'test', 'test_internet'],
+        help="The operation to perform")
+    parser.add_argument('--vm', required=False, 
+        help="Virtual Machine where execute the operation")
+    parser.add_argument('-p', '--pool', default=2, type=int, 
+        help="This is the number of parallel process (default 2)")
+    args = parser.parse_args()
 
-        # shut down network
-        if args.action == "update":
-            os.system('sudo ./net_enable.sh')
-            print "[!] Enabling NETWORKING!"
-        else:
-            os.system('sudo ./net_disable.sh')
-            print "[!] Disabling NETWORKING!"
+    if args.action == "test":
+        get_results("eset")
+        exit(0)
 
-        vm_conf_file = os.path.join("conf", "vms.cfg")
-        op_conf_file = os.path.join("conf", "operations.cfg")
+    # shut down network
+    if args.action == "update":
+        os.system('sudo ./net_enable.sh')
+        print "[!] Enabling NETWORKING!"
+    else:
+        os.system('sudo ./net_disable.sh')
+        print "[!] Disabling NETWORKING!"
 
-        # get configuration for AV update process (exe, vms, etc)
+    vm_conf_file = os.path.join("conf", "vms.cfg")
+    op_conf_file = os.path.join("conf", "operations.cfg")
 
-        vmman = VMManagerVS(vm_conf_file)
+    # get configuration for AV update process (exe, vms, etc)
 
-        # get vm names
-        c = ConfigParser()
-        c.read(op_conf_file)
-        vm_names = c.get("test", "machines").split(",")
+    vmman = VMManagerVS(vm_conf_file)
 
-        pool = Pool(int(args.pool))
+    # get vm names
+    c = ConfigParser()
+    c.read(op_conf_file)
+    vm_names = c.get("test", "machines").split(",")
 
-        print "[*] selected operation %s" % args.action
+    pool = Pool(int(args.pool))
 
-        actions = { "update" : update, "revert": revert, 
-                    "dispatch": dispatch, "test_internet": test_internet }
-        r = pool.map_async(actions[args.action], vm_names)
-        print "[*] RESULTS: "
-        print r.get()
+    print "[*] selected operation %s" % args.action
+
+    actions = { "update" : update, "revert": revert, 
+                "dispatch": dispatch, "test_internet": test_internet }
+    r = pool.map_async(actions[args.action], vm_names)
+    print "[*] RESULTS: "
+    print r.get()
 
 
 if __name__ == "__main__":
