d997a0577c621aad24ab332ffa60d2117727c114
Zeno <zeno@hackingteam.com>
2013-03-04 14:02:35 +0100

    fixed

--- snippet/dynamic_calls.py
+++ snippet/dynamic_calls.py
@@ -1,20 +1,21 @@
 class vm:
 	def __init__(self):
-		print "diopor"
+		print "portacilapace"
 
 	def power_on(self):
 		return "vm is powered on"
 
 class manager:
 	def open_vm(self):
-		self.vm = vm
+		self.vm = vm()
 
 	def __getattr__(self, name):
 		f = getattr(self.vm, name)
-		f()
+		return f
 
 if __name__ == "__main__":
 
 	m = manager()
 	m.open_vm()
-	m.power_on()
+	s = m.power_on()
+	print s
