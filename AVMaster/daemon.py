2e879cd0f4077f3b27b36f7b24a2dba44797261f
olli <olli@hackingteam.it>
2013-06-20 12:05:57 +0200

    removed init_db calls
    problems with devel and prod versions and MySQL

--- AVMaster/daemon.py
+++ AVMaster/daemon.py
@@ -1,6 +1,6 @@
 import sys
 
-from lib.web.app import DB_PATH, init_db, app
+from lib.web.app import app #DB_PATH, init_db, app
 
 def init_web():
 	port = 8000
@@ -8,7 +8,7 @@ def init_web():
 	if len(sys.argv) == 2:
 		port = int(sys.argv[1]) 
 	
-	init_db(DB_PATH)
+#	init_db(DB_PATH)
 	app.run(host='0.0.0.0', port=port)
 
 def main():
