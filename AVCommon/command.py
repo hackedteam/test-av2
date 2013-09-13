d666f8460e8103b96e318e61e229bad90c0c14fb
Zeno <zeno@hackingteam.com>
2013-09-13 17:51:47 +0200

    Command con from e to

--- AVCommon/command.py
+++ AVCommon/command.py
@@ -2,14 +2,15 @@ from communicator import CommunicatorRedisList
 
 class Command:
     def __init__(self, frm=None, to=None, host="localhost"):
+        self.frm = frm
         if frm is not None:
-            self.receive = CommunicatorRedisList(host, frm)
+            self.receive_from = CommunicatorRedisList(host, frm)
         if to is not None:
-            self.send = CommunicatorRedisList(host, to)
+            self.send_to = CommunicatorRedisList(host, to)
 
     def send(self, cmd):
-        self.send.write([frm,cmd])
+        self.send_to.write([self.frm, cmd])
 
     def receive(self):
-        received = self.receive.read()
+        received = self.receive_from.read()
         return received
