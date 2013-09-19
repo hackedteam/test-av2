805679372d396ce531dfa550795da11eb858cb5d
Zeno <zeno@hackingteam.com>
2013-09-19 12:39:13 +0200

    logging all tests green

---
+++ AVCommon/test_logging.py
@@ -0,0 +1,23 @@
+import logging, sys
+import logging.config
+
+import test_logging_child
+
+
+formatter = logging.Formatter('%(asctime)s -%(levelname)s- %(filename)s:%(lineno)s   %(message)s')
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(formatter)
+logger=logging.getLogger('')
+logger.addHandler(stdout_handler)
+logger.setLevel(logging.DEBUG)
+logging.debug('A debug message')
+logging.info('Some information')
+logging.warning('A shot across the bows')
+
+logging.config.fileConfig('../logging.conf')
+logging.debug('A debug message')
+logging.info('Some information')
+
+logging.warning('A shot across the bows')
+
+test_logging_child.ClassName("calling a child")
---
+++ AVCommon/test_logging_child.py
@@ -0,0 +1,10 @@
+import logging
+
+
+class ClassName(object):
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+        logging.debug("Hello world: %s", arg)
+
