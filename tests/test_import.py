ae16c585dbffa886b67869a0e089496713beec98
Zeno <zeno@hackingteam.it>
2013-11-03 20:44:20 +0100

    test import
    Signed-off-by: Zeno <zeno@hackingteam.it>

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,1 +1,17 @@
 __author__ = 'fabrizio'
+import os, sys
+import logging
+import logging.config
+
+def test_import():
+    prev = os.path.join(os.getcwd(), "..")
+    if not prev in sys.path:
+        sys.path.append(prev)
+
+
+    sys.path.append("../AVCommon")
+    import AVCommon
+    from AVCommon import MQ
+
+if __name__ == '__main__':
+    logging.config.fileConfig('logging.conf')
