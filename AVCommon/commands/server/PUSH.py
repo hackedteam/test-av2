2b80e783131f41de7a86261241c7735e00190813
Zeno <zeno@hackingteam.it>
2013-11-13 17:46:04 +0100

    fixed
    Signed-off-by: Zeno <zeno@hackingteam.it>

--- AVCommon/commands/server/PUSH.py
+++ AVCommon/commands/server/PUSH.py
@@ -16,37 +16,42 @@ def execute(vm, args):
         src_dir = ""
     assert isinstance(src_files, list), "PUSH expects a list of src files"
 
-    memo = []
     all_src = []
+    relative_parents = set()
 
     """ look if i need all files in one directory """
     for src_file in src_files:
         g = glob.glob(os.path.join(src_dir,src_file))
         print g
         for f in g:
-            all_src.append(f.replace("/%s" % src_dir,""))
+            # s is the relative file, expanded by glob
+            s = f.replace("%s/" % src_dir,"")
+            all_src.append(s)
 
-    relative_parents = set()
-    for s in all_src:
-        p = os.path.split(s)[0]
-        while p and p != src_dir:
-            relative_parents .add(p)
-            p = os.path.split(p)[0]
+            # add all the parents to the relative_parents set, to avoid repetitions
+            p = os.path.split(s)[0]
+            while p and p != src_dir:
+                relative_parents.add(p)
+                p = os.path.split(p)[0]
 
+    # sorts the parents by length, so that parent is always before its sons
     parents = list(relative_parents)
     parents.sort(lambda x,y: len(x) - len(y))
+    logging.debug("parents: %s" % parents)
 
-    for rdir in parents:
+    ntdir = lambda x: x.replace("/","\\")
+
+    for r in parents:
+        rdir = ntdir(os.path.join(dst_dir, r))
         logging.debug("mkdir %s " % (rdir))
         vm_manager.execute(vm, "mkdirInGuest", rdir)
-        memo.append(rdir)
 
     """ then upload parsed files """
     logging.debug("All files to copy are:\n%s" % src_files)
     for src_file in all_src:
 
         src = os.path.join(src_dir, src_file)
-        dst = os.ntpath.join(dst_dir, src_file)
+        dst = ntdir(os.path.join(dst_dir, src_file))
 
         logging.debug("%s copy %s -> %s" % (vm, src, dst))
         r = vm_manager.execute(vm, "copyFileToGuest", src, dst)
