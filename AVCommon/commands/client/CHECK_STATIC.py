20bf8bfe79fb2495b497adb92341f88d9512eead
Zeno <zeno@hackingteam.it>
2013-11-19 13:30:59 +0100

    test av agent
    Signed-off-by: Zeno <zeno@hackingteam.it>

---
+++ AVCommon/commands/client/CHECK_STATIC.py
@@ -0,0 +1,17 @@
+__author__ = 'zeno'
+
+
+import logging
+import time
+
+from AVCommon import command
+from AVAgent import build
+
+def on_init(vm, args):
+    pass
+
+def on_answer(vm, success, answer):
+    pass
+
+def execute(vm, args):
+    pass
