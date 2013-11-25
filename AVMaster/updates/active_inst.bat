8dd6334ba3e441d243b830d992787d632b6c6b8d
olli <olli@hackingteam.it>
2013-11-25 13:48:01 +0100

    new procedures TEST_PY and UPDATE_PYTHON for automatic install and check of AVAgent

--- AVMaster/updates/active_inst.bat
+++ AVMaster/updates/active_inst.bat
@@ -1,3 +1,4 @@
 msiexec /i c:\Users\avtest\Desktop\active_py.msi
 
-C:\Python27\scripts\pip.exe install pyyaml redis
+C:\Python27\scripts\pip.exe install pyyaml
+C:\Python27\scripts\pip.exe install redis
