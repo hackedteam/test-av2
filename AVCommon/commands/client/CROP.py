3d1cd2abfb15c11d4d74188010bed33c2fee2064
Zeno <zeno@hackingteam.com>
2013-12-05 17:31:59 +0100

    CHECK_ELITE

--- AVCommon/commands/client/CROP.py
+++ AVCommon/commands/client/CROP.py
@@ -97,3 +97,6 @@ def crop(iter):
         logging.debug("actual crop save: %s" % name)
         logging.debug("name: %s size: %s" % ( name, c.size ))
         c.save(name)
+        return True
+
+    return False
