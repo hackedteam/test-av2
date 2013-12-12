c9cc9767d93a404b409e3f52bf5c43d71f3dc6b0
Zeno <zeno@hackingteam.it>
2013-12-12 09:25:13 +0100

    TODO: kill rcs agent
    Signed-off-by: Zeno <zeno@hackingteam.it>

---
+++ AVCommon/commands/client/KILL_RCS.py
@@ -0,0 +1,21 @@
+__author__ = 'fabrizio'
+
+from AVCommon.logger import logging
+
+import AVCommon
+import AVAgent
+
+
+def on_init(protocol, args):
+    """ server side """
+    pass
+
+
+def on_answer(vm, success, answer):
+    """ server side """
+    pass
+
+
+def execute(vm, args):
+    #TODO
+    pass
