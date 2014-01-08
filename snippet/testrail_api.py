79494031ada6a6447bb10987e93b571b967f9753
Zeno <zeno@hackingteam.com>
2014-01-08 14:03:37 +0100

    experimenting

--- snippet/testrail_api.py
+++ snippet/testrail_api.py
@@ -3,6 +3,7 @@ import urllib2
 from urllib2 import HTTPError
 import sys
 import base64
+import pprint
 
 user_hash = base64.encodestring('apitestrail@hackingteam.com:apicoltore').replace('\n', '')
 base_url = "http://172.20.20.168/testrail/index.php?api"
@@ -19,13 +20,15 @@ def send_get(url):
         return "HTTPError found: %s" % e
     return resp
 
-def send_post(url,data):
+def send_post(url, data):
     try:
-        req = urllib2.Request(url,data)
-        req.add_header("Accept","application/json")
+        req = urllib2.Request(url,json.dumps(data))
+        req.add_header("Content-Type","application/json")
+        req.add_header("Authorization","Basic %s" % user_hash)
         f = urllib2.urlopen(req)
-        resp = f.read()
-    except HTTPError:
+        ret = f.read()
+        resp = json.loads(ret)
+    except HTTPError as e:
         return "HTTPError found: %s" % e
     return resp
 
@@ -42,14 +45,53 @@ def get_plan(proj_id,plan_id):
         if p["id"] == plan_id:
             return p
 
+def add_plan(project_id, plan):
+    add_plan_url = "%s/v2/add_plan/%d" % (base_url, project_id)
+    return send_post(add_plan_url, plan)
+
+def add_plan_entry(plan_id, plan_entry):
+    add_plan_url = "%s/v2/add_plan_entry/%d" % (base_url, plan_id)
+    return send_post(add_plan_url, plan_entry)
+
+def rerun_plan(project_id,plan_id):
+    plan = get_plan(project_id, plan_id)
+    runs =  get_runs(project_id, plan_id)
+
+    new_plan = {}
+    new_plan["name"] = plan["name"] + " AUTO"
+    #new_plan["entries"] = runs
+    new_plan["description"] = "automatic plan"
+    new_plan["milestone_id"] = plan["milestone_id"]
+    new_plan_ret = add_plan(project_id, new_plan)
+
+    pprint.pprint(new_plan_ret)
+
+    new_plan_id = new_plan_ret["id"]
+    print new_plan_id
+    for r in runs:
+        add_plan_entry(new_plan_id, r)
+
+    #close_plan(plan_id)
+    return new_plan_id
 
 def main():
     project_id = 1
-    plan = get_plan(project_id,55)
-    print "%s: %s" % (plan["name"], plan["description"])
+    plan_id = 274
+    plan = get_plan(project_id, plan_id)
+    print  "%s: %s" % (plan["name"], plan["description"])
+    pprint.pprint(plan)
 
     print "Getting Runs for %s" % plan["name"]
-    print get_runs(project_id, plan["id"])
+    #pprint.pprint( get_runs(project_id, plan["id"]))
+    runs =  get_runs(project_id, plan["id"])
+    for r in runs:
+        #print r
+        #break
+        if r["id"]== 85:
+            pprint.pprint(r)
+
+    new_plan_id = rerun_plan(project_id, plan_id)
+
 
 if __name__ == "__main__":
     main()
