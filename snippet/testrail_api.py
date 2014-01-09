eed0be97d55e00b0f95f3502c70a52a6ba9f1553
olli <olli@hackingteam.it>
2014-01-09 09:45:12 +0100

    added IOBit vm to list, continuing integration with TestRail

--- snippet/testrail_api.py
+++ snippet/testrail_api.py
@@ -25,9 +25,8 @@ def send_post(url, data):
         req = urllib2.Request(url,json.dumps(data))
         req.add_header("Content-Type","application/json")
         req.add_header("Authorization","Basic %s" % user_hash)
-        f = urllib2.urlopen(req)
-        ret = f.read()
-        resp = json.loads(ret)
+        f = urllib2.urlopen(req).read()
+        resp = json.loads(f)
     except HTTPError as e:
         return "HTTPError found: %s" % e
     return resp
@@ -40,10 +39,23 @@ def get_runs(proj_id,plan_id):
     get_runs_url = "%s/v2/get_runs/%d/%s" % (base_url,proj_id,plan_id)
     return send_get(get_runs_url)
 
+def get_tests(run_id):
+    get_tests_url = "%s/v2/get_tests/%d" % (base_url, run_id)
+    return send_get(get_tests_url)
+
 def get_plan(proj_id,plan_id):
     for p in get_plans(proj_id):
         if p["id"] == plan_id:
             return p
+
+def get_run(proj_id, plan_id, r_id):
+    for r in get_runs(proj_id, plan_id):
+        if r["id"] == r_id:
+            return r
+
+def get_results(t_id):
+    get_results_url = "%s/v2/get_results/%d" % (base_url, t_id)
+    return send_get(get_results_url)
 
 def add_plan(project_id, plan):
     add_plan_url = "%s/v2/add_plan/%d" % (base_url, project_id)
@@ -52,6 +64,10 @@ def add_plan(project_id, plan):
 def add_plan_entry(plan_id, plan_entry):
     add_plan_url = "%s/v2/add_plan_entry/%d" % (base_url, plan_id)
     return send_post(add_plan_url, plan_entry)
+
+def add_result(test_id, result):
+    add_result_url = "%s/v2/add_result/%d" % (base_url, test_id)
+    return send_post(add_result_url, result)
 
 def rerun_plan(project_id,plan_id):
     plan = get_plan(project_id, plan_id)
@@ -64,8 +80,6 @@ def rerun_plan(project_id,plan_id):
     new_plan["milestone_id"] = plan["milestone_id"]
     new_plan_ret = add_plan(project_id, new_plan)
 
-    pprint.pprint(new_plan_ret)
-
     new_plan_id = new_plan_ret["id"]
     print new_plan_id
     for r in runs:
@@ -76,20 +90,33 @@ def rerun_plan(project_id,plan_id):
 
 def main():
     project_id = 1
-    plan_id = 274
+    plan_id = 55
     plan = get_plan(project_id, plan_id)
-    print  "%s: %s" % (plan["name"], plan["description"])
-    pprint.pprint(plan)
-
-    print "Getting Runs for %s" % plan["name"]
+    '''
+    #print  "%s: %s" % (plan["name"], plan["description"])
+    #pprint.pprint(plan)
+    #return
     #pprint.pprint( get_runs(project_id, plan["id"]))
+    #run = get_run(project_id, plan["id"], 275)
+    #pprint.pprint(run)
+    print "Getting Runs for %s (%s)" % (plan["name"], plan["id"])
     runs =  get_runs(project_id, plan["id"])
+    #pprint.pprint(runs)
     for r in runs:
-        #print r
-        #break
-        if r["id"]== 85:
-            pprint.pprint(r)
+        if r["config"]== "avg":
+            #pprint.pprint(r)
+            for t in get_tests(r["id"]):
+                if "Update" in  t["title"]:
+                    print "Searching stuff for AVG"
+                    #r = get_results(t["id"])
+                    res = {"status_id": "4"}
+                    #add_result(t["id"],res)
+                    #r = get_results(t["id"])
+                    #print t["title"], r[0]["status_id"]
+#            pprint.pprint(  )
 
+            print "---"
+    '''
     new_plan_id = rerun_plan(project_id, plan_id)
 
 
