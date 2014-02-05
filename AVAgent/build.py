43e6b33a4ac358ec60df8b65c0364ee228ad3a2c
Zeno <zeno@hackingteam.com>
2014-02-05 09:35:57 +0100

    Ottimo giro di update e daily

--- AVAgent/build.py
+++ AVAgent/build.py
@@ -283,11 +283,7 @@ class AgentBuild:
                 exe = new_exe
 
             logging.debug("- Execute: " + exe)
-<<<<<<< HEAD
             subp = subprocess.Popen([exe]) #, shell=True)
-=======
-            subp = subprocess.Popen([exe])
->>>>>>> feb39b519498693f004d4807ae15b85b94885130
             add_result("+ SUCCESS SCOUT EXECUTE")
         except Exception, e:
             logging.debug("DBG trace %s" % traceback.format_exc())
@@ -372,7 +368,7 @@ class AgentBuild:
             ret = info['upgradable'] is False
 
             if ret:
-                add_result("+ SUCCESS UPGRADED SYNC: %s" % info['level'])
+                add_result("+ SUCCESS UPGRADED SYNC")
             else:
                 add_result("+ NOT YET UPGRADED SYNC: %s" % info['level'])
 
