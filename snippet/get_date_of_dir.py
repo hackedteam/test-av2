6ca9f58ba9a3c6576b22a7ff38343b9bf465d719
olli <olli@hackingteam.it>
2014-03-05 12:01:03 +0100

    added last vms to rite configuration. autoupdate snippet fixed some paths

--- snippet/get_date_of_dir.py
+++ snippet/get_date_of_dir.py
@@ -4,14 +4,16 @@ import os, sys, time
 
 dir = {}
 
-dir['avast']  = ( "C:\\Program Files\\AVAST Software\\Avast", "defs" )
-dir['avg']    = ( "C:\\ProgramData\\AVG2014", "DB" )
-dir['avira']  = ( "C:\\ProgramData\\Avira\\AntiVir Desktop", "UPDATE" )
-dir['comodo'] = ( "C:\\ProgramData\\COMODO\\Cis\\cmc2", "local_trees" )
-dir['kav']    = ( "C:\ProgramData\Kaspersky Lab\AVP14.0.0\Data\updater\supd_b90b2edf", "updater.xml" )
-dir['mcafee'] = ( "C:\\ProgramData\McAfee\\MSC\\Updates", "Download" )
-dir['norton'] = ( "C:\\Program Files (x86)\\Norton Internet Security\\NortonData", "21.1.0.18" )
-dir['panda']  = ( "C:\\Program Files (x86)\\Panda Security\\Panda Antivirus Pro 2014", "Downloads" )
+dir['avast']  = ( "C:\\Program Files\\AVAST Software\\Avast", "defs" ) # OK
+dir['comodo'] = ( "C:\\ProgramData\\COMODO\\Cis\\cmc2", "local_trees" ) # OK
+dir['kis14']    = ( "C:\\ProgramData\\Kaspersky Lab\\AVP14.0.0\\Data", "updater" ) # IT WORKS
+dir['mcafee'] = ( "C:\\ProgramData\McAfee\\MSC\\Updates", "Download" ) # OK
+dir['norton'] = ( "C:\\Program Files (x86)\\Norton Internet Security\\NortonData\\21.1.0.18\\Definitions", "VirusDefs" ) #OK
+dir['panda']  = ( "C:\\Program Files (x86)\\Panda Security\\Panda Internet Security 2013", "Downloads" ) # OK
+
+dir['avg']    = ( "C:\\ProgramData\\AVG2013\\IDS", "malwareprofile" )            # WRONG DIRECTORY
+dir['avira']  = ( "C:\\ProgramData\\Avira\\AntiVir Desktop", "UPDATE" ) # WRONG DIR
+
 
 def get_date_from_dir(dirpath, pname):
     entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
@@ -22,7 +24,7 @@ def get_date_from_dir(dirpath, pname):
     #NOTE: use `ST_MTIME` to sort by a modification date
 
     for cdate, path in sorted(entries):
-        print time.ctime(cdate), os.path.basename(path)
+        print time.ctime(os.path.getmtime(path)), os.path.basename(path)
 
 if __name__ == "__main__":
 
