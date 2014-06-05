f1fe364e44f5ea2a4dddb24d50ea78d7844562b2
Marco Losito <m.losito@hackingteam.com>
2014-06-05 18:07:28 +0200

    Correzione PUSH con path particolari
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

--- AVCommon/commands/server/PUSH.py
+++ AVCommon/commands/server/PUSH.py
@@ -33,29 +33,43 @@ def execute(vm, protocol, args):
         g = glob.glob(os.path.join(src_dir, src_file))
         if not g:
             logging.warn("Empty glob")
+        # if you arrive here, then you already found the file on the filesystem.
+        # typically the file have a relative path
         for f in g:
             # s is the relative file, expanded by glob
             s = f.replace("%s/" % src_dir, "")
             all_src.append(s)
+            #logging.debug("file completo f: %s, file relativo s: %s" % (f,s))
 
+            #logging.debug("Check if exists file %s" % f)
             assert os.path.exists(f), "%s %s" % (f, os.getcwd())
-            assert os.path.exists(s), "%s %s" % (s, os.getcwd())
+
+            #inserito da Marco
+            #logging.debug("Check if exists file %s" % os.path.join(src_dir, s))
+            assert os.path.exists(os.path.join(src_dir, s)), "%s %s" % (s, os.getcwd())
 
             # add all the parents to the relative_parents set, to avoid repetitions
             p = os.path.split(s)[0]
             while p and p != src_dir:
                 relative_parents.add(p)
+                #print("1_relative parents")
                 p = os.path.split(p)[0]
 
     # sorts the parents by length, so that parent is always before its sons
+
+    relative_parents.add("./")
     parents = list(relative_parents)
     parents.sort(lambda x, y: len(x) - len(y))
     logging.debug("parents: %s" % parents)
 
     ntdir = lambda x: x.replace("/", "\\")
 
+    print(parents)
+
     for r in parents:
+        #print(dst_dir)
         rdir = ntdir(os.path.join(dst_dir, r))
+        #print("2_crea cartelle %s" % rdir)
         logging.debug("mkdir %s " % (rdir))
         vm_manager.execute(vm, "mkdirInGuest", rdir)
 
@@ -65,8 +79,11 @@ def execute(vm, protocol, args):
         return False, "Empty file list"
 
     for src_file in all_src:
+        #print("3_processa file")
         src = os.path.join(src_dir, src_file)
         dst = ntdir(os.path.join(dst_dir, src_file))
+
+        #logging.debug("Check if exists file %s" % src)
 
         if not os.path.exists(src):
             return False, "Not existent file: %s" % src
@@ -74,7 +91,7 @@ def execute(vm, protocol, args):
             pass
             #logging.debug("file exists")
 
-        #logging.debug("%s copy %s -> %s" % (vm, src, dst))
+        logging.debug("%s copy %s -> %s" % (vm, src, dst))
         r = vm_manager.execute(vm, "copyFileToGuest", src, dst)
 
         if r > 0:
--- assets/python/active_inst.bat
+++ assets/python/active_inst.bat
@@ -1,4 +1,4 @@
-msiexec /i c:\Users\avtest\Desktop\active_py.msi
+msiexec /i c:\avtest\assets\python\active_py.msi
 
 C:\Python27\scripts\pip.exe install pyyaml
 C:\Python27\scripts\pip.exe install redis
