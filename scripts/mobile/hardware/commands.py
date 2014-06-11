792c525e686266c68992502b1a922b0dcbe7cf91
olli <olli@hackingteam.it>
2014-06-11 17:53:21 +0200

    added file commands with function build

---
+++ scripts/mobile/hardware/commands.py
@@ -0,0 +1,33 @@
+__author__ = 'olli'
+
+sys.path.append("/Users/olli/Documents/work/AVTest/")
+from AVAgent import build
+
+def set(srv_params, device_params):
+    pass
+
+def build(kind):
+    class Args:
+        pass
+
+    report = ""
+
+    args = Args()
+
+    args.action = action
+    args.platform = platform
+    args.kind = kind
+    args.backend = backend
+    args.frontend = frontend
+    args.param = param
+    args.blacklist = blacklist
+    args.soldierlist = soldierlist
+    args.platform_type = platform_type
+    args.nointernetcheck = nointernetcheck
+    args.operation = operation
+    args.puppet = puppet
+
+    results, success, errors = build.build(args, report)
+def check_evidences():
+    pass
+
