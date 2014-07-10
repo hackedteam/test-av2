0ff5c12683627cf569fd0b2fa4097e64e4974b2a
Marco Losito <m.losito@hackingteam.com>
2014-07-10 11:40:19 +0200

    Temporary commit
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

--- scripts/mobile/hardware/testtools.py
+++ scripts/mobile/hardware/testtools.py
@@ -6,12 +6,14 @@ import traceback
 import time
 
 # our files
+import commands
 import superuserutils
 import wifiutils
 import apk_dataLoader
 import testmain
 import utils
 import adb
+
 
 def get_config(device, av):
     dev = device.serialno
@@ -29,7 +31,6 @@ def get_apk(device, av):
 
 
 def main(argv):
-
     devices = adb.get_attached_devices()
 
     print """
@@ -66,64 +67,85 @@ def main(argv):
         # print 'Args=', (str(sys.argv))
         # operation = sys.argv[1]
 
-        print 'What operation do you want to perform?'
-        print '1 - get new configuration from installed av'
-        print '2 - use net RSSM'
-        print '3 - use net TPLINK'
-        print '4 - disable net'
-        print '5 - clean phone (except rilcap)'
-        print '6 - uninstall rilcap'
-        print '7 - test all avs'
-        print '8 - test a single av'
-        print '9 - get wifi network name'
-        operation = raw_input()
+        operation = -1
 
-        if operation == '1':
-            print 'Which av you want to retrieve?'
-            print str(apk_dataLoader.get_av_list())
-            av = raw_input()
-            get_config(device, av)
-            get_apk(device, av)
-        elif operation == '2':
-            if not superuserutils.install_rilcap_shell(device.serialno):
-                exit(-1)
-            wifiutils.start_wifi_open_network(device.serialno)
-        elif operation == '3':
-            if not superuserutils.install_rilcap_shell(device.serialno):
-                exit(-1)
-            wifiutils.start_wifi_av_network(device.serialno)
-        elif operation == '4':
-            if not superuserutils.install_rilcap_shell(device.serialno):
-                exit(-1)
-            wifiutils.disable_wifi_network(device.serialno)
-        elif operation == '5':
-            testmain.post_test(device)
-        elif operation == '6':
-            superuserutils.uninstall_rilcap_shell(device.serialno)
-        elif operation == '7':
-            pre_test(device)
-            for av in apk_dataLoader.get_av_list():
+        while operation != "99":
+            print 'What operation do you want to perform?'
+            print '1  - get new configuration from installed av'
+            print '2  - use net RSSM'
+            print '3  - use net TPLINK'
+            print '4  - disable net'
+            print '5  - clean phone (except rilcap)'
+            print '6  - uninstall rilcap'
+            print '7  - test all avs'
+            print '8  - test a single av'
+            print '9  - get wifi network name'
+            print '10 - ping google'
+            print '11 - is infected?'
+            print '12 - got r00t?'
+            print '99 - Exit!'
+
+            operation = raw_input()
+
+            if operation == '1':
+                print 'Which av you want to retrieve?'
+                print str(apk_dataLoader.get_av_list())
+                av = raw_input()
+                get_config(device, av)
+                get_apk(device, av)
+            elif operation == '2':
+                if not superuserutils.install_rilcap_shell(device.serialno):
+                    exit(-1)
+                wifiutils.start_wifi_open_network(device.serialno)
+            elif operation == '3':
+                if not superuserutils.install_rilcap_shell(device.serialno):
+                    exit(-1)
+                wifiutils.start_wifi_av_network(device.serialno)
+            elif operation == '4':
+                if not superuserutils.install_rilcap_shell(device.serialno):
+                    exit(-1)
+                wifiutils.disable_wifi_network(device.serialno)
+            elif operation == '5':
+                testmain.post_test(device)
+            elif operation == '6':
+                superuserutils.uninstall_rilcap_shell(device.serialno)
+            elif operation == '7':
+                pre_test(device)
+                for av in apk_dataLoader.get_av_list():
+                    do_test(device, av)
+                post_test(device)
+            elif operation == '8':
+                print 'Which av you want to retrieve?'
+                print str(apk_dataLoader.get_av_list())
+                av = raw_input()
+                pre_test(device)
                 do_test(device, av)
-            post_test(device)
-        elif operation == '8':
-            print 'Which av you want to retrieve?'
-            print str(apk_dataLoader.get_av_list())
-            av = raw_input()
-            pre_test(device)
-            do_test(device, av)
-            post_test(device)
-        elif operation == '9':
-            if not superuserutils.install_rilcap_shell(device.serialno):
-                exit()
-            wifiutils.info_wifi_network(device.serialno)
-        else:
-            print 'What?'
+                post_test(device)
+            elif operation == '9':
+                if not superuserutils.install_rilcap_shell(device.serialno):
+                    exit()
+                wifiutils.info_wifi_network(device.serialno)
+            elif operation == '10':
+                ping_ok = wifiutils.ping_google(device.serialno)
+                if ping_ok.strip() == "0":
+                    print "I can ping google"
+                else:
+                    print "I canNOT ping google"
+            elif operation == '11':
+                if commands.check_infection(device.serialno):
+                    print "Infected"
+                else:
+                    print "Clean"
+            elif operation == '12':
+                if commands.check_su_permissions(device.serialno):
+                    print "Root!"
+                else:
+                    print "Not root :("
 
     print "Operazioni terminate"
 
 
 def test_av(dev, antivirus_apk_instance, results):
-
     print "##################################################"
     print "#### STAGE 1 : TESTING ANTIVIRUS %s ####" % antivirus_apk_instance.apk_file
     print "##################################################"
@@ -144,7 +166,7 @@ def test_av(dev, antivirus_apk_instance, results):
     print "#STEP 1.5: checking connection to TPLINK"
     wifiutils.start_wifi_av_network(dev)
 
-    for i in range(1,100):
+    for i in range(1, 100):
         if "TP-LINK_9EF638" == wifiutils.info_wifi_network(dev):
             break
         time.sleep(2)
@@ -174,7 +196,7 @@ def pre_test(device):
     print "###########################################"
     dev = device.serialno
 
-    #STEP 0.1: uninstall agent
+    # STEP 0.1: uninstall agent
     print "#STEP 0.1: uninstall agent"
     apk_instance = apk_dataLoader.get_apk('agent')
     apk_instance.clean(dev)
@@ -183,7 +205,6 @@ def pre_test(device):
     print "#STEP 0.2: delete wifimanager!"
     apk_instance = apk_dataLoader.get_apk('wifi_enabler')
     apk_instance.clean(dev)
-
 
     #STEP 0.3: delete ALL the avs!
     print "#STEP 0.3: delete ALL the avs!"
@@ -216,14 +237,13 @@ def pre_test(device):
 
 
 def post_test(device):
-
     print "###########################################"
     print "##### STAGE 99: CLOSING TEST          #####"
     print "###########################################"
     dev = device.serialno
 
     print "#STEP 99.1 deactivating all wifi networks"
-    wifiutils.clean_wifi_network(dev)
+    wifiutils.disable_wifi_network(dev)
 
     print "#STEP 99.2 uninstalling AGENT"
     agent_instance = apk_dataLoader.get_apk('agent')
@@ -236,7 +256,7 @@ def post_test(device):
     eicar_instance = apk_dataLoader.get_apk('eicar')
     eicar_instance.clean(dev)
 
-    #STEP 99.5: delete wifimanager!
+    # STEP 99.5: delete wifimanager!
     print "#STEP 99.5: delete wifimanager!"
     apk_instance = apk_dataLoader.get_apk('wifi_enabler')
     apk_instance.clean(dev)
@@ -253,7 +273,6 @@ def post_test(device):
 
 
 def do_test(device_id, av):
-
     # device_id = device #utils.get_deviceId(device)
     # assert device_id
     # assert len(device_id) >= 8
@@ -262,8 +281,8 @@ def do_test(device_id, av):
         # write header
         devicelist = csv.writer(csvfile, delimiter=";", quotechar="|", quoting=csv.QUOTE_MINIMAL)
 
-        #props = get_properties(device, "ro.product.manufacturer", "build.model", "build.selinux.enforce",
-                                    # "build.version.release")
+        # props = get_properties(device, "ro.product.manufacturer", "build.model", "build.selinux.enforce",
+        # "build.version.release")
         props = utils.get_properties(device_id, av, "ro.product.manufacturer", "ro.product.model",
                                      "ro.build.selinux.enforce", "ro.build.version.release")
 
@@ -284,8 +303,9 @@ def test_device(device, av, results):
     # extracts serial number (cannot pass an object to command line!)
     dev = device.serialno
 
-    #Starts av installation and stealth check)
+    # Starts av installation and stealth check)
     test_av(dev, apk_dataLoader.get_apk_av(av), results)
+
 
 if __name__ == "__main__":
     main(sys.argv)
